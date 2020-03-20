72ba7b660e1682b060c0aa295375307d3f81d445
rodrigosegatto <Segattorodrigo@hotmail.com>
2020-03-19 21:43:28 -0300

    1ª Parte do curso (Conceitos ES6). 5ª Operadores Rest e Spreed, e também Template Literals

---
+++ main_template_literals.js
@@ -0,0 +1,8 @@
+const nome = 'Rodrigo';
+const idade = 27;
+
+//console.log('Meu nome é '+ nome + ' e tenho '+ idade + ' anos');
+
+//Esta é uma funcionalidade conhecida como TemplateLiterals do ES6
+//Facilita na concatenação, deixa o código mais limpo
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
