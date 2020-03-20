86bf7c53487c8bb0e36f9ad4ef0744e9db6e0649
rodrigosegatto <Segattorodrigo@hotmail.com>
2020-03-20 20:58:55 -0300

    4ª Parte do Curso. 2ª aula, adicionar repositorios - js estatico ainda

--- src/main.js
+++ src/main.js
@@ -1,34 +1,26 @@
-//Import de função normalmente
-    //import {soma as somaFunction,sub} from './funcoes';
-
-//import de função default
-//Tem somente um método por default e podemos chama-lo através de qualquer nome
-    //import somaFunction from './soma';
-
-
-//Import de todas as funcoes do .js que poderão ser chamadas funcoes.soma()
-    //import * as funcoes from './funcoes';
-
-//console.log(somaFunction(1,2));
-//console.log(sub(5,4));
-
-//IMport de Classe
-    //import ClasseUsuario from './funcoes';
-
-    //ClasseUsuario.info();
-
-//Utilização do axios para requisições com ES6
-    import axios from 'axios';
-
-    class Api {
-        static async getUserInfo(username){
-            try{
-                const response = await axios.get(`https://api.github.com/users/${username}`);
-                console.log(response);
-            }catch(err){
-                console.warn('Erro na API');
-            }
-        }
+class App {
+    constructor (){
+        this.repositories = [];
+        this.formElement = document.getElementById('repo-form');
+        this.registerHandlers();
     }
 
-    Api.getUserInfo('rodrigosegatto');
+    registerHandlers(){
+        this.formElement.onsubmit = event => this.addRepository(event); 
+    }
+
+    addRepository(event){
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire sua ideia do papel e de vida a sua startup',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'http://github.com/rocketseat/rocketseat.com.br'
+        });
+
+        console.log(this.repositories);
+    }
+}
+
+new App();
