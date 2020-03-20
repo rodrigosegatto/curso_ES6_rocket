bf43b1fdbfaa79c4dae06bbece2716540b15ff96
rodrigo_segatto <Segattorodrigo@hotmail.com>
2020-03-20 15:20:33 -0300

    2ª Parte do Curso. Fim desta parte do curso. Iniciando 3ª Parte

---
+++ 2-Webpack-server/src/main.js
@@ -0,0 +1,17 @@
+//Import de função normalmente
+    //import {soma as somaFunction,sub} from './funcoes';
+
+//import de função default
+//Tem somente um método por default e podemos chama-lo através de qualquer nome
+    //import somaFunction from './soma';
+
+
+//Import de todas as funcoes do .js que poderão ser chamadas funcoes.soma()
+    //import * as funcoes from './funcoes';
+
+//console.log(somaFunction(1,2));
+//console.log(sub(5,4));
+
+import ClasseUsuario from './funcoes';
+
+ClasseUsuario.info();
