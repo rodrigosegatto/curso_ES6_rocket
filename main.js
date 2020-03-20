8b861a342d5b90a28efd5b46cf3f6088a91ddb1a
rodrigo_segatto <Segattorodrigo@hotmail.com>
2020-03-20 13:37:13 -0300

    2ª parte do curso. Aula sobre Import/Exports

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-import {soma} from './funcoes';
+//Import de função normalmente
+import {soma as somaFunction,sub} from './funcoes';
 
-console.log(soma(1,2));
+//import de função default
+//Tem somente um método por default e podemos chama-lo através de qualquer nome
+//import somaFunction from './soma';
+
+
+//Import de todas as funcoes do .js que poderão ser chamadas funcoes.soma()
+//import * as funcoes from './funcoes';
+
+console.log(somaFunction(1,2));
+console.log(sub(5,4));
